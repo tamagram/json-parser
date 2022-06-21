e9b52881063cd1090a63e4c5aa98ca5949fd85c4
tamagram <tamadevelopment211@gmail.com>
2022-06-22 04:37:33 +0900

    :white_check_mark: Pass test

--- parser/parser.py
+++ parser/parser.py
@@ -15,3 +15,28 @@ def parse_array(tokens):
         arr.append(t)
         tokens = tokens[1:]
     raise Exception("Excepted end-of-string RIGHT_BRACKET")
+
+
+def parse_object(tokens):
+    t = tokens[0]
+    if t != LEFT_BRACE:
+        return None, tokens
+
+    tokens = tokens[1:]
+    obj = {}
+    while 0 < len(tokens):
+        t = tokens[0]
+        if t == RIGHT_BRACE:
+            return obj, tokens[1:]
+
+        key = tokens.pop(0)
+        if type(key) is not str:
+            raise Exception("Expected string key, got: {}".format(key))
+
+        colon = tokens.pop(0)
+        if colon != COLON:
+            raise Exception('Expected colon after key in object, got: {}'.format(colon))
+        
+        value = tokens.pop(0)
+        obj[key] = value
+    raise Exception('Expected end-of-object bracket')
