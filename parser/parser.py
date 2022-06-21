35f5cddc4a960a0da0ffd85ef34a5e92789db765
tamagram <tamadevelopment211@gmail.com>
2022-06-22 03:38:43 +0900

    :white_check_mark: Pass test

--- parser/parser.py
+++ parser/parser.py
@@ -1,13 +1,17 @@
-# from parser.constant import *
+from parser.constant import *
 
-# def parse_array(tokens):
-#     t = tokens[0]
-#     if t != LEFT_BRACKET:
-#         return None
 
-#     arr = []
-#     while 0 < len(tokens):
-#         if t == RIGHT_BRACKET:
-#             return arr, tokens[1:]
-#         arr.append(t)
-#         tokens = tokens[1:]
+def parse_array(tokens):
+    t = tokens[0]
+    if t != LEFT_BRACKET:
+        return None, tokens
+
+    tokens = tokens[1:]
+    arr = []
+    while 0 < len(tokens):
+        t = tokens[0]
+        if t == RIGHT_BRACKET:
+            return arr, tokens[1:]
+        arr.append(t)
+        tokens = tokens[1:]
+    raise Exception("Excepted end-of-string RIGHT_BRACKET")
