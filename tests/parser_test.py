0f3dc6f286b6138cb22bd41c0fdb08b752baa3bc
tamagram <tamadevelopment211@gmail.com>
2022-06-22 03:38:19 +0900

    :white_check_mark: Add test

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,10 +1,23 @@
-# import unittest
-# from parser.parser import *
+import unittest
+from parser.parser import *
 
 
-# class TestParseArray(unittest.TestCase):
-#     def test_array(self):
-#         arg = ["[", "a", "b", "c", "]"]
-#         want = ["a", "b", "c"]
-#         got = parse_array(arg)
-#         self.assertEqual(want, got)
+class TestParseArray(unittest.TestCase):
+    def test_array(self):
+        arg = ["[", "a", "b", "c", "]"]
+        want = {"json_array": ["a", "b", "c"], "tokens": []}
+        json_array, tokens = parse_array(arg)
+        got = {"json_array": json_array, "tokens": tokens}
+        self.assertEqual(want, got)
+
+    def test_object(self):
+        arg = ["{", "user", ":", "hoge", "}"]
+        want = {"json_array": None, "tokens": ["{", "user", ":", "hoge", "}"]}
+        json_array, tokens = parse_array(arg)
+        got = {"json_array": json_array, "tokens": tokens}
+        self.assertEqual(want, got)
+
+    def test_exception(self):
+        arg = ["[", "a", "b"]
+        with self.assertRaises(Exception):
+            parse_array(arg)
